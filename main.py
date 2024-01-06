a799ac88223a5c0738258aab76038fb3cc699b6a
dwchoi <justicejustice314@gmail.com>
2024-01-06 11:55:27 +0900

    <fix> pop

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ while True:
     currentPrice = publicApiClient.getCurrentPrice("BTC")
 
     for i in range(1, 6):
-        buyOrder = privateApiClient.buyOrder(price=int(currentPrice - btcUnitPrice * i * 4),
+        buyOrder = privateApiClient.buyOrder(price=int(currentPrice - btcUnitPrice * i * 1),
                                              unit=(remainingKrw/5) / currentPrice)
-        sellOrder = privateApiClient.sellOrder(price=int(currentPrice + btcUnitPrice * i * 4), unit=remainingBtc / 5)
-        if type(buyOrder) is tuple:
-            orderList.append(buyOrder)
-        if type(sellOrder) is tuple:
-            orderList.append(sellOrder)
+        sellOrder = privateApiClient.sellOrder(price=int(currentPrice + btcUnitPrice * i * 1), unit=remainingBtc / 5)
+        orderList.append(buyOrder)
+        orderList.append(sellOrder)
 
-    for orders in orderList:
-        privateApiClient.cancelOrder(orders)
+    while len(orderList) > 0:
+        privateApiClient.cancelOrder(orderList.pop())
